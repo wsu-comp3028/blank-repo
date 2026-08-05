f27320c74057e5f5ab664348ec0b44d5ab30b03e
namnguyenvt <nguyennam11022005@gmail.com>
2026-08-05 02:07:45 +0000

    Just keep same thing in remote branch

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// Hello World in Germany
+console.log("Hallo, Welt!");
+
+// Hello World in Spanish
+console.log("Ciao, Mondo!");
+
+// Hello World in English
+console.log("Hello, World!");
+
+// Hello World in French
+console.log("Bonjour le Monde!");
 
 // Factorial function
 function main() {
